fcc8ace4f1ae2f442aab961c650dd8934c531be9
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-16 20:05:11 +0530

    Unit Tests

---
+++ BDMSUnitTests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+
+namespace BDMSUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "RexrG6OuogoAu6dghwvsZtLkCi3z5ig2NfaRtnsK",
+            BasePath = "https://blood-donor-9af63-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        public void TestMethod1()
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(config);
+
+                if (client != null)
+                {
+
+                }
+                else
+                {
+                    Console.Write("Check your connection again!");
+                }
+            }
+            catch (Exception)
+            {
+                Console.Write("Check your connection again!", "Error");
+            }
+        }
+    }
+}
